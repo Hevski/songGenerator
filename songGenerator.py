72723664396753588f50102401746bf06d2e6571
Heather Davies <heatherdavies@Heathers-MacBook-Pro.local>
2018-06-03 12:03:24 +0100

    first commit

---
+++ songGenerator.py
@@ -0,0 +1,10 @@
+word = raw_input("What word do you want to add? ")
+
+def sing(text, word):
+    song = text + word
+    print song
+    return song
+
+sing("Boom boom shake shake the ", word)
+sing("If I die before I ",word)
+sing("whooooooooo mysterious ", word)
